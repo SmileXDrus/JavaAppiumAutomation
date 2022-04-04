3b6403e60aaf6d22d39b99103e9aaa247bb0d4bf
SmileXDrus <vovamuh@yandex.ru>
2022-04-04 18:41:02 +0300

    ex_2

--- src/exercises/MainClass.java
+++ src/exercises/MainClass.java
@@ -1,6 +1,11 @@
 package exercises;
 
 public class MainClass {
+    private final int class_number = 20;
+    public int getClassNumber() {
+        return class_number;
+    }
+
     public int getLocalNumber() {
         return (5 + 9);
     }
--- src/exercises/MainClassTest.java
+++ src/exercises/MainClassTest.java
@@ -7,6 +7,11 @@ public class MainClassTest  {
     MainClass mc_obj = new MainClass();
 
     @Test
+    public void testGetClassNumber(){
+        Assert.assertTrue("Number not more than 45", mc_obj.getClassNumber() > 45);
+    }
+
+    @Test
     public void testGetLocalNumber() {
         Assert.assertEquals("Return value is not equal 14", 14, mc_obj.getLocalNumber());
     }
